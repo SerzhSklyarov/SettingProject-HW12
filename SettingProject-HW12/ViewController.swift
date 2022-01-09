73479fed5c15c9ea297295f5a2e3aabb7dc241c6
Sergey Sklyarov <sklyarov.it.020@gmail.com>
2022-01-09 21:30:39 +0100

    Initial Commit

---
+++ SettingProject-HW12/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SettingProject-HW12
+//
+//  Created by Wizzard Sklyarov on 2022. 01. 09..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
