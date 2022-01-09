82dcb66d3ec2180818f2c0c2cc46603e918bfba0
Sergey Sklyarov <sklyarov.it.020@gmail.com>
2022-01-09 23:25:48 +0100

    Create cells.

---
+++ SettingProject-HW12/Views/StatusCell.swift
@@ -0,0 +1,59 @@
+//
+//  StatusCell.swift
+//  SettingProject-HW12
+//
+//  Created by Wizzard Sklyarov on 2022. 01. 09..
+//
+
+import Foundation
+import UIKit
+
+class StatusCell: ParentCell {
+    static let identifier = "StatusCell"
+    
+    private let statusLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .systemGray
+        label.numberOfLines = 1
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        accessoryType = .none
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func setupHierarchy() {
+        super.setupHierarchy()
+        contentView.addSubview(statusLabel)
+    }
+    
+    override func setupLayout() {
+        super.setupLayout()
+        NSLayoutConstraint.activate([
+            statusLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            statusLabel.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: -20)
+        ])
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        iconBox.backgroundColor = nil
+        iconImage.image = nil
+        cellLabel.text = nil
+        statusLabel.text = nil
+    }
+    
+    func configure(with model: StatusCellModel) {
+        iconBox.backgroundColor = model.iconColor
+        iconImage.image = model.icon
+        cellLabel.text = model.title
+        statusLabel.text = model.status
+    }
+}
