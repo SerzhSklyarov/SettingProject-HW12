efcef7b78f3791ca7c65b191298cb146a9f1bcec
Sergey Sklyarov <sklyarov.it.020@gmail.com>
2022-01-12 16:05:32 +0100

    Relocate init() and required init().

--- SettingProject-HW12/Views/ParentCell.swift
+++ SettingProject-HW12/Views/ParentCell.swift
@@ -39,6 +39,16 @@ class ParentCell: UITableViewCell {
         return label
     }()
     
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupHierarchy()
+        setupLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     // MARK: - Setup
     
     func setupHierarchy() {
@@ -65,16 +75,6 @@ class ParentCell: UITableViewCell {
             cellLabel.leftAnchor.constraint(equalTo: iconBox.rightAnchor, constant: 25)
         ])
         
-    }
-    
-    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
-        setupHierarchy()
-        setupLayout()
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
     }
     
     // MARK: - Metrics
--- SettingProject-HW12/Views/SimpleCell.swift
+++ SettingProject-HW12/Views/SimpleCell.swift
@@ -11,12 +11,6 @@ import UIKit
 class SimpleCell: ParentCell {
     static let identifier = "SimpleCell"
     
-    func configure(with model: SimpleCellModel) {
-        iconBox.backgroundColor = model.iconColor
-        iconImage.image = model.icon
-        cellLabel.text = model.title
-    }
-    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         accessoryType = .disclosureIndicator
@@ -24,6 +18,12 @@ class SimpleCell: ParentCell {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure(with model: SimpleCellModel) {
+        iconBox.backgroundColor = model.iconColor
+        iconImage.image = model.icon
+        cellLabel.text = model.title
     }
     
     override func prepareForReuse() {
